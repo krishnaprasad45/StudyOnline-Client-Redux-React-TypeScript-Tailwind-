259430519a688917a8860f1949419f9092bf6ec4
krishna prasad <krishnaprasadc45@gmail.com>
2024-01-30 23:45:46 +0530

    Chat feature working fine

--- src/components/Common/Chat/ChatBody.tsx
+++ src/components/Common/Chat/ChatBody.tsx
@@ -4,6 +4,8 @@ import { socket } from "../../../services/socket.io/socketConfig";
 import { RootState } from "../../../Interfaces/common";
 import { useSelector } from "react-redux";
 import userEndpoints from "../../../Constraints/endpoints/userEndpoints";
+import mentorEndpoints from "../../../Constraints/endpoints/mentorEndpoints";
+import { mentorAxios } from "../../../Constraints/axiosInterceptors/mentorAxiosInterceptors";
 
 interface Message {
   message: string;
@@ -13,27 +15,31 @@ interface Message {
 }
 interface ChatBodyProps {
   role: string;
-  chatId:string |undefined;
-  email:string |undefined;
+  chatId: string | undefined;
+  email: string | undefined;
 }
 
 const ChatBody: React.FC<ChatBodyProps> = (props) => {
-
   const navigate = useNavigate();
   const [messages, setMessages] = useState<Message[]>([]);
   const [newMessage, setNewMessage] = useState<string>("");
   const userStore = useSelector((state: RootState) => state.user);
   let mentorEmail: string | undefined;
+  const chatId = props.chatId;
+  const role = props.role;
   if (userStore) {
     mentorEmail = userStore?.user?.mentorIncharge;
   }
-  console.log("props",props)
+  console.log("props", props);
 
   useEffect(() => {
+    mentorAxios
+      .get(`${mentorEndpoints.getChatHistory}?chatId=${chatId}`)
+      .then((response) => {
+        console.log("res-data", response.data);
+        setMessages(response.data.messages);
+      });
   
-
-    
-
     socket.on("SentMessage", (msg) => {
       setMessages((prevMessages) => {
         if (!prevMessages.find((m) => m.id === msg.id)) {
@@ -42,9 +48,13 @@ const ChatBody: React.FC<ChatBodyProps> = (props) => {
         return prevMessages;
       });
     });
-
-   
-  }, []);
+  
+    // Scroll to the bottom when messages are updated
+    const chatContainer = document.getElementById("chat-container");
+    if (chatContainer) {
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  }, [messages]);
 
   const handleLeaveChat = () => {
     localStorage.removeItem("userName");
@@ -55,7 +65,7 @@ const ChatBody: React.FC<ChatBodyProps> = (props) => {
   };
 
   const handleSendMessage = (e: FormEvent) => {
-    console.log("handle sed message 6")
+    console.log("handle sed message 6");
     e.preventDefault();
     if (
       (messages && localStorage.getItem("userEmail")) ||
@@ -64,10 +74,10 @@ const ChatBody: React.FC<ChatBodyProps> = (props) => {
       const messageData = {
         from: props.role,
         message: newMessage,
-        to: props.role == 'user' ? "mentor" : "user",
-        id: props.chatId,
-      }
-      console.log("message data to serever ",messageData)
+        to: role == "user" ? "mentor" : "user",
+        id: chatId,
+      };
+      console.log("message data to serever ", messageData);
       socket.emit("SentMessage", messageData);
     }
     setNewMessage("");
@@ -76,62 +86,82 @@ const ChatBody: React.FC<ChatBodyProps> = (props) => {
   return (
     <div className=" ml-80 w-full  ">
       {/* ChatBar */}
-      
-      { mentorEmail === "not assigned" ? (
-        <p className="  text-center text-orange-600 py-4 ">You need to purchase the course to unlock the chat feature.
-        <button
+
+      {mentorEmail === "not assigned" ? (
+        <p className="  text-center text-orange-600 py-4 ">
+          You need to purchase the course to unlock the chat feature.
+          <button
             onClick={handleOnclick}
             className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-blue-600 rounded-lg focus:ring-4 focus:outline-none"
           >
             Buy Course
-          </button></p>
-      ) :(
-       <div >
-      <header className="chat__mainHeader">
-        <p>CHAT WINDOW</p>
-        <button className="leaveChat__btn" onClick={handleLeaveChat}>
-          LEAVE CHAT
-        </button>
-      </header>
-
-      <div>
-        <div className="message__container bg-violet-50">
-          {messages.map((data) =>
-            data.from === localStorage.getItem("userEmail") ? (
-              <div className="message__chats" key={data.id}>
-                <p className="sender__name">You</p>
-                <div className="message__sender">
-                  <p>{data.message}</p>
-                </div>
-              </div>
-            ) : (
-              <div className="message__chats" key={data.id}>
-                <p>{data.from}</p>
-
-                <div className="message__recipient">
-                  <p>{data.message}</p>
-                </div>
-              </div>
-            )
-          )}
-        </div>
-
-        <div className="chat__footer">
-          <form className="form" onSubmit={handleSendMessage}>
-            <input
-              type="text"
-              placeholder="Write message"
-              className="message"
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-            />
-            <button type="submit" className="sendBtn bg-lime-500">
-              SEND
+          </button>
+        </p>
+      ) : (
+        <div>
+          <header className="chat__mainHeader">
+            <p>CHAT WINDOW</p>
+            <button className="leaveChat__btn" onClick={handleLeaveChat}>
+              LEAVE CHAT
             </button>
-          </form>
+          </header>
+
+          <div>
+            <div  id="chat-container" className="message__container bg-violet-50">
+              {messages.map((data) =>
+                role == "mentor" ? (
+                  data.from === "mentor" ? (
+                    <div className="message__chats" key={data.id}>
+                      <p className="sender__name">You</p>
+                      <div className="message__sender">
+                        <p>{data.message}</p>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="message__chats" key={data.id}>
+                      <p>{data.from}</p>
+
+                      <div className="message__recipient">
+                        <p>{data.message}</p>
+                      </div>
+                    </div>
+                  )
+                ) : data.from === "mentor" ? (
+                  <div className="message__chats" key={data.id}>
+                    <p>{data.from}</p>
+
+                    <div className="message__recipient">
+                      <p>{data.message}</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="message__chats" key={data.id}>
+                    <p className="sender__name">You</p>
+                    <div className="message__sender">
+                      <p>{data.message}</p>
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+
+            <div className="chat__footer">
+              <form className="form" onSubmit={handleSendMessage}>
+                <input
+                  type="text"
+                  placeholder="Write message"
+                  className="message"
+                  value={newMessage}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                />
+                <button type="submit" className="sendBtn bg-lime-500">
+                  SEND
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>)}
+      )}
     </div>
   );
 };
