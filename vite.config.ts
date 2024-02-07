08c85b2e689674938e3614386f277ea32ad2a6c7
krishna prasad <krishnaprasadc45@gmail.com>
2024-02-07 17:30:46 +0530

    fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       util: "@browsery/util",
       process: "process/browser",
     },
