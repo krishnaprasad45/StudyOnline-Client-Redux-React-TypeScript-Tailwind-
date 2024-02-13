c7f1c64e303a7182919c18ac280f2f67780480cd
krishna prasad <krishnaprasadc45@gmail.com>
2024-02-13 21:36:19 +0530

    moving

--- src/services/socket.io/socketConfig.ts
+++ src/services/socket.io/socketConfig.ts
@@ -2,9 +2,9 @@ import { io } from "socket.io-client";
 
 
 // const URL = import.meta.env.VITE_APIURL;
-const URL = 'http://localhost:4000'
+const url = 'http://localhost:4000'
 console.log("Url",URL)
-export const socket = io(URL, {
+export const socket = io(url, {
     autoConnect: true,
     withCredentials: true,
 });
